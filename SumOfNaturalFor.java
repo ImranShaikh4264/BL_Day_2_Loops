f7c5f328d1f39b1f853fa8cb28ecf5fcecf7b119
ImranShaikh4264 <shaikhimrana56@gmail.com>
2022-11-01 23:18:46 +0530

    Added: Find sum of natural number using for loop

---
+++ SumOfNaturalFor.java
@@ -0,0 +1,12 @@
+package com.bridgelab.day2.loops;
+
+public class SumOfNaturalFor {
+	public static void main(String[] args) {
+		int number = 10, sum = 0;
+		for ( int i = 1; i <= number; ++i )
+			sum += i;
+	
+
+    System.out.println("sum = " + sum);
+}
+}
