6fc6c2d70b3c332078d5820ebc334c03b17b2253
ImranShaikh4264 <shaikhimrana56@gmail.com>
2022-11-01 23:15:15 +0530

    Added: Reverse given number using while loop

---
+++ ReverseIntegerWhile.java
@@ -0,0 +1,19 @@
+package com.bridgelab.day2.loops;
+
+import java.util.Scanner;
+
+public class ReverseIntegerWhile {
+	public static void main(String[] args)  {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter number");
+	int Integer = sc.nextInt(); 
+	int reverse = 0;
+	System.out.println("Given Integer is : "+Integer);
+		while(Integer != 0)   {  
+			int remainder = Integer % 10;  
+			reverse = reverse * 10 + remainder;  
+			Integer = Integer/10;  
+		}  
+	System.out.println("The reverse of the given Integer is: " + reverse);  
+	}  
+}
